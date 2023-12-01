61684b4b68281be3cf200bfde6e4c8cd38367bdf
Thalles Rafael <thallesrafaell@outlook.com>
2023-11-30 21:50:03 -0300

    ADICIOANDO TESTE DE COMENTARIOS

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,32 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from "@testing-library/react";
+import PostComment from ".";
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+describe("Teste para o componente PostComment", () => {
+  it("Deve renderizar o componente corretamente", () => {
+    render(<PostComment />);
+    expect(screen.getByText("Comentar")).toBeInTheDocument();
+  });
+
+  test("Deve renderizar comentario corretamente", () => {
+    render(<PostComment />);
+    const form = screen.getByTestId("btn-adicionar-comentario");
+    const comments = screen.getByTestId("txt-comments");
+
+    fireEvent.change(comments, {
+      target: {
+        value: "Legal a miniatura",
+      },
     });
+    fireEvent.submit(form);
+
+    fireEvent.change(comments, {
+      target: {
+        value: "Muito real , que demais",
+      },
+    });
+    fireEvent.submit(form);
+
+    const result = screen.getAllByTestId("item-comment");
+    expect(result).toHaveLength(2);
+  });
 });
